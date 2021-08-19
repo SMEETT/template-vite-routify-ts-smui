86738dfe0a02d227d8f86f27c801843a0807693b
SMEETT <tbr@tutamail.com>
2021-08-19 15:06:10 +0200

    initial commit

---
+++ svts_bl/src/pages/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let fun: number = 10;
+	import Counter from "@/lib/Counter.svelte";
+	console.log("testing some stuff");
+</script>
+
+<div class="border">
+	<p>Hello</p>
+	TS-World @ Vite!! {fun}
+</div>
+<Counter />
+
+<style lang="scss">
+	.border {
+		border: 1px solid red;
+		p {
+			color: red;
+			font-size: 20rem;
+		}
+	}
+</style>
---
+++ svts_bl/vite.config.js
@@ -0,0 +1,26 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import path from "path";
+const __dirname = path.resolve(path.dirname(""));
+
+export default defineConfig({
+	server: {
+		port: 5000,
+	},
+	build: {
+		cssCodeSplit: false,
+		sourcemap: true,
+	},
+	optimizeDeps: {
+		exclude: ["@roxi/routify"],
+	},
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: path.resolve(__dirname, "./src"),
+			},
+		],
+	},
+	plugins: [svelte()],
+});
